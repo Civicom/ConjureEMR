e34156251375d070ac2fd2c3a5384a2ddc4f0ac0
Charles92011 <charles.johnson@civi.com>
2025-01-24 08:54:11 -0800

    WIP creates admin page, removes employees page

--- packages/telemed-ehr/app/src/pages/Admin.tsx
+++ packages/telemed-ehr/app/src/pages/Admin.tsx
@@ -3,7 +3,7 @@ import { TabsList, TabsTrigger, TabsContent, Tabs } from '@/components/ui/tabs';
 import EmployeesTable from '@/shadcn/components/EmployeesTable';
 import useOttehrUser, { OttehrUser } from '../hooks/useOttehrUser';
 import { RoleType } from '@/types/types';
-import { BriefcaseBusiness, UserPen, MapPinned, Settings } from 'lucide-react';
+
 export default function AdminPage(): ReactElement {
     
     const user = useOttehrUser();
@@ -13,35 +13,15 @@ export default function AdminPage(): ReactElement {
     return (
         <div className="flex flex-col max-w-7xl mx-auto my-16 px-4">
             <div className="space-y-8">
-                <Tabs defaultValue="employees" className="w-full">
-                    <TabsList className="w-full">
-                        <TabsTrigger value="employees" className="flex justify-center text-center">
-                            <div className="flex flex-col">
-                                <UserPen className="mx-auto my-auto text-[#4b5c6b] w-[30px] h-[30px]" />
-                                <p className="text-[16px] font-semibold text-[#4b5c6b]">Employees</p>
-                            </div>
-                        </TabsTrigger>
-                        <TabsTrigger value="insurance" className="flex justify-center text-center">
-                            <div className="flex flex-col">
-                                <BriefcaseBusiness className="mx-auto my-auto text-[#4b5c6b] w-[30px] h-[30px]" />
-                                <p className="text-[16px] font-semibold text-[#4b5c6b]">Insurance</p>
-                            </div>
-                        </TabsTrigger>
-                        <TabsTrigger value="states" className="flex justify-center text-center">
-                            <div className="flex flex-col">
-                                <MapPinned className="mx-auto my-auto text-[#4b5c6b] w-[30px] h-[30px]" />
-                                <p className="text-[16px] font-semibold text-[#4b5c6b]">States</p>
-                            </div>                            
-                        </TabsTrigger>
-                        <TabsTrigger value="settings" className="flex justify-center text-center">
-                            <div className="flex flex-col">
-                                <Settings className="mx-auto my-auto text-[#4b5c6b] w-[30px] h-[30px]" />
-                                <p className="text-[16px] font-semibold text-[#4b5c6b]">Settings</p>
-                            </div>                                   
-                        </TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="employees" className="my-8"><EmployeesTable /></TabsContent>
-                </Tabs>
+            <Tabs defaultValue="employees" className="w-full">
+                <TabsList className="w-full">
+                    <TabsTrigger value="employees" className="flex justify-center text-center">Employees</TabsTrigger>
+                    <TabsTrigger value="insurance" className="flex justify-center text-center">Insurance</TabsTrigger>
+                    <TabsTrigger value="states" className="flex justify-center text-center">States</TabsTrigger>
+                    <TabsTrigger value="settings" className="flex justify-center text-center">Settings</TabsTrigger>
+                </TabsList>
+                <TabsContent value="employees" className="my-8"><EmployeesTable /></TabsContent>
+            </Tabs>
             </div>
         </div>
     );
